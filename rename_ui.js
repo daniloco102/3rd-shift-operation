ee39e92255e0e4f03243652b262a642753f0c0f8
danloco102 <danloco102@users.noreply.github.com>
2026-08-01 02:45:48 +0700

    Dọn dẹp file nháp

--- rename_ui.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('index.html', 'utf8');
-
-html = html.replace('Danh Sách Chi Tiết', 'Dữ Liệu Toàn Bộ Nhân Sự (Danh Sách Chi Tiết)');
-html = html.replace('<option value="all">Tất cả tiêu chí</option>', '<option value="all">Toàn bộ nhân sự (Tất cả)</option>');
-
-fs.writeFileSync('index.html', html);
-console.log('Replaced successfully');
